41d0bfca57600ecc109a332756dd50277a4f9376
Kit Glover <kglover@atlassian.com>
2025-10-07 13:36:39 +1100

    Fix tests

--- src/resolvers/services/messageService.test.js
+++ src/resolvers/services/messageService.test.js
@@ -4,7 +4,7 @@ describe('MessageService', () => {
   describe('getSecretMessage', () => {
     it('should return the correct secret message', () => {
       const result = getSecretMessage();
-      expect(result).toBe('This is a secret message.');
+      expect(result).toBe('This is a secret message. Do not tell anyone!!');
     });
 
     it('should return a string type', () => {
